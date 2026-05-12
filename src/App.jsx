de4f56f3cda27d632de4a704ef5aadba98363472
Amal Biju <amalbiju.aluva@gmail.com>
2026-05-12 21:02:49 +0530

    Reapply "added a cube animation"
    This reverts commit 9d04b6abc72e2818171a1576cd37d33519463383.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Scene from "./components/Scene";
 
 function App() {
   return(
     <>
+    <div className="scroll-smooth">
     <Navbar />
-    <Hero />
-    <About />
-    <Projects />
-    <Contact />
+
+    <section id="hero">
+      <Hero />
+    </section>
+
+    <section id="about">
+      <About />
+    </section>
+
+    <section id="projects">
+      <Projects />
+    </section>
+
+    <section id="contact">
+      <Contact />
+    </section>
+    
     <Footer />
+    </div>
     </>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
 function Contact() {
     return(
-        <section  class="flex flex-col items-center justify-center p-6 bg-gray-100 text-gray-800">
-            <h2 class="text-3xl font-bold mb-4">Contact Me</h2>
-            <p class="text-lg">Feel free to reach out to me!</p>
+        <section  className="flex flex-col items-center justify-center p-6 bg-gray-100 text-gray-800">
+            <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
+            <p className="text-lg">Feel free to reach out to me!</p>
         </section>
     )
 }
